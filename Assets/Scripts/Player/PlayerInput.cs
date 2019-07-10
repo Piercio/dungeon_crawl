5e37136fe66404eb0790eafe17c44209ff103e98
piercio <piercio@loggi.com>
2019-07-09 22:04:31 -0300

    first level working

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// class to manage all player keyboard input
+public class PlayerInput : MonoBehaviour {
+
+    private PlayerMovement playerMovement;		// reference to PlayerMove script
+
+	// Use this for initialization
+	void Awake () {
+        playerMovement = GetComponent<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!playerMovement.IsMoving()) {
+        	// if player is not moving (return value of IsMoving is false)
+            GetPlayerInput();
+        }
+	}
+
+	// function to manage player keyboard input
+    private void GetPlayerInput()
+    {
+		// variables of horizontal moving, vertical moving and turning
+        int horizontal = 0;
+        int vertical = 0;
+        int turning = 0;
+
+		// cache movement and turning values based on input axis
+        horizontal = (int)(Input.GetAxisRaw("Horizontal"));
+        vertical = (int)(Input.GetAxisRaw("Vertical"));
+        turning = (int)(Input.GetAxisRaw("Turning"));
+
+		// limit movement and turning values so that only one value is something than zero
+        if (horizontal != 0) {
+            playerMovement.MovePlayer(horizontal, 0, 0);
+        } else if (vertical != 0) {
+            playerMovement.MovePlayer(0, vertical, 0);
+        } else if (turning != 0) {
+            playerMovement.MovePlayer(0, 0, turning);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MovingUnit {
+
+	PlayerHealth playerHealth;
+
+	// Use this for initialization
+    protected override void Awake () {
+    	base.Awake();
+    	playerHealth = GetComponent<PlayerHealth>();
+    }
+	
+	public void MovePlayer(int hor, int ver, int rot) {
+		if (enabled) {
+			Vector3 endPos = Vector3.zero;
+
+			if (ver != 0) {
+				endPos = transform.position + transform.forward * ver;
+				base.StartMove(endPos);
+				playerHealth.TakeDamage(1);
+			} else if (hor != 0) {
+				endPos = transform.position + transform.right * hor;
+				base.StartMove(endPos);
+			} else if (rot != 0) {
+				base.StartRotation(rot);
+			}
+		}
+	}
+
+	protected override void CannotMove<T>(T component) {
+		Debug.Log("blocked!");
+	}
+}
