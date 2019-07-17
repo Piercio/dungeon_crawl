d5ebe8e1a7d80e0376e7da5153b7675d70bdf43f
piercio <piercio@loggi.com>
2019-07-17 10:36:58 -0300

    DialogueSystem

---
+++ Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour {
+
+	public static DialogueSystem Instance { get; set; }
+
+	public delegate void OnDialogueFinish();
+	
+	[SerializeField] GameObject dialoguePanel;
+	[SerializeField] Button okButton;
+	[SerializeField] Text dialogueText;
+
+	int dialogueIndex;
+	List<string> dialogueLines;
+	OnDialogueFinish finishCallback;
+    
+    void Awake() {
+    	if (Instance != null && Instance != this) {
+    		Destroy(gameObject);
+    	} else {
+    		Instance = this;
+    	}
+
+        dialoguePanel.SetActive(false);
+        okButton.onClick.AddListener(delegate { ContinueDialogue(); });
+    }
+
+    public void AddNewDialogue(List<string> lines, OnDialogueFinish callback) {
+    	dialogueIndex = 0;
+    	dialogueLines = lines;
+		finishCallback = callback;
+        CreateDialogue();
+    }
+
+    void CreateDialogue() {
+    	dialogueText.text = dialogueLines[dialogueIndex];
+    	dialoguePanel.SetActive(true);
+    }
+
+    void ContinueDialogue() {
+    	dialogueIndex++;
+    	if (dialogueIndex < dialogueLines.Count) {
+    		dialogueText.text = dialogueLines[dialogueIndex];
+    	} else {
+    		dialoguePanel.SetActive(false);
+    		finishCallback();
+    	}
+    }
+}
---
+++ Assets/Scripts/NPCs/Altar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Altar : Interactable {
+
+	List<string> dialogueLines = new List<string>(2);
+	bool firstTime = true;
+
+	void Start() {
+		dialogueLines.Add("Pray at the Altar?");
+		dialogueLines.Add("Everytime you pray the state of the world resets. Some enemies and bosses will not respawn");
+	}
+
+	public override void Interact(Transform transform) {
+		DialogueSystem.OnDialogueFinish callback = StartPraying;
+		DialogueSystem.Instance.AddNewDialogue(dialogueLines, callback);
+	}
+
+	public void StartPraying() {
+		if (firstTime) {
+			dialogueLines.RemoveAt(1);
+			firstTime = false;
+		}
+		Debug.Log("Player is praying!");
+	}
+}
